b082b60561d31d77c49d366b5ab4eec21f445951
alberto <alberto.taddeucci@gmail.com>
2024-02-02 15:48:44 +0100

    algoritmo snack 4

---
+++ Snack 4/script.js
@@ -0,0 +1,24 @@
+/*
+
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+
+*/
+
+
+
+
+const input = Number(prompt(""));
+
+let stringNumb = input.toString();
+
+let sum = 0;
+
+for (let i = 0; i < stringNumb.length; i++){
+    sum += Number(stringNumb[i]);
+
+}
+
+
+document.getElementById("result").innerText = sum
+
