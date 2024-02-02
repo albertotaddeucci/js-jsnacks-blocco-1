98b73f99c7a652ca2e9a89fa9195e6b17657a4a6
alberto <alberto.taddeucci@gmail.com>
2024-02-02 14:26:23 +0100

    algoritmo snack #2

---
+++ Snack 2/script.js
@@ -0,0 +1,19 @@
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+
+let sum = 0;
+for (i=0; i<10; i++){
+    
+    const input =  Number(prompt("inserisci numero"));
+    sum += input;
+
+}
+
+document.getElementById("result").innerText = sum
+
+
+
+
