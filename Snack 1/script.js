59eba8cd89e9c03e32f32fbd0fedb9fcc3b9a697
alberto <alberto.taddeucci@gmail.com>
2024-02-02 14:07:54 +0100

    algoritmo snack 1

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -1,1 +1,23 @@
+const inputName1 = prompt("inserisci primo nome");
+const inputName2 = prompt("inserisci secondo nome");
+
+const lenghtName1 = inputName1.length;
+const lenghtName2 = inputName2.length;
+
+console.log(lenghtName1)
+
+if (lenghtName1>lenghtName2){
+    document.getElementById("result").innerText = inputName1 + inputName2
+
+} else if (lenghtName1<lenghtName2) {
+    document.getElementById("result").innerText = inputName2 + inputName1
+
+} else {
+    document.getElementById("result").innerText = "le parole sono dela stessa lunghezza"
+    
+}
+
+
+
+
 
